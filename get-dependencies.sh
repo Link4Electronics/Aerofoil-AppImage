5b3970f5f82ba29d6f608f305043ba790e272076
Link4Electronics <link4electronics@gmail.com>
2026-02-03 03:14:22 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -19,8 +19,6 @@ get-debloated-pkgs --add-common --prefer-nano
 #make-aur-package
 
 # If the application needs to be manually built that has to be done down here
-#mv -v ./src/aerofoil-git/build/Packaged ./AppDir/lib/aerofoil/
-
 echo "Making nightly build of Aerofoil..."
 echo "---------------------------------------------------------------"
 REPO="https://github.com/elasota/Aerofoil"
@@ -33,5 +31,3 @@ mkdir -p build && cd build
 cmake .. -DCMAKE_INSTALL_PREFIX="/usr" -DCMAKE_BUILD_TYPE=Release
 make -j$(nproc)
 make install
-cp "../Resources/Linux/io.github.elasota.aerofoil.desktop" "/usr/share/applications"
-cp "../Resources/Linux/io.github.elasota.aerofoil.svg" "/usr/share/pixmaps"
