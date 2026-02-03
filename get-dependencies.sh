909a8dea66e96c2614159fdde45dbfed3e035c8c
Link4Electronics <link4electronics@gmail.com>
2026-02-03 02:25:50 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,14 +6,35 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm
+pacman -Syu --noconfirm \
+    cmake      \
+    freetype2  \
+    sdl2       \
+    sdl2_image
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
 # Comment this out if you need an AUR package
-make-aur-package
+#make-aur-package
 
 # If the application needs to be manually built that has to be done down here
 #mv -v ./src/aerofoil-git/build/Packaged ./AppDir/lib/aerofoil/
+
+echo "Making nightly build of Aerofoil..."
+echo "---------------------------------------------------------------"
+REPO="https://github.com/elasota/Aerofoil"
+VERSION="$(git ls-remote "$REPO" HEAD | cut -c 1-9 | head -1)"
+git clone --recursive --depth 1 "$REPO" ./Aerofoil
+echo "$VERSION" > ~/version
+
+cd ./Aerofoil
+mkdir -p build && cd build
+cmake .. -DCMAKE_INSTALL_PREFIX="/usr" -DCMAKE_BUILD_TYPE=Release
+make -j$(nproc)
+make install
+#mkdir -p "/usr/share/applications"
+#mkdir "/usr/share/pixmaps"
+cp "Resources/Linux/io.github.elasota.aerofoil.desktop" "/usr/share/applications"
+cp "Resources/Linux/io.github.elasota.aerofoil.svg" "/usr/share/pixmaps"
